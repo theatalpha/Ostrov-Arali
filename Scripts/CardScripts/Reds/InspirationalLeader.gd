3da941ebccd70de09a8ffa6b9e0fb0097e233148
Ginga17 <39541891+Ginga17@users.noreply.github.com>
2018-06-23 23:40:59 +1000

    Add files via upload

---
+++ Scripts/CardScripts/Reds/InspirationalLeader.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var atk = 2
+var hp = 9
+var CardName = "Inspirational Leader"
+var CardType = "melee"
+
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+func _process(delta):
+	get_node("AttackDisplay").set_text(str(atk))
+	get_node("HealthDisplay").set_text(str(hp))
+	get_node("NameDisplay").set_text(str(CardName))
+
+	pass
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
