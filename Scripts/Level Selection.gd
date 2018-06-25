dfa88b9fbb8d48b6c4c12b1dcbe0a67a5bb25fee
rediMAX101 <39573997+rediMAX101@users.noreply.github.com>
2018-06-25 18:39:27 +1000

    Add files via upload

--- Scripts/Level Selection.gd
+++ Scripts/Level Selection.gd
@@ -1,25 +1,21 @@
 extends Node2D
 
-#var button1_location = Position2D(530, 895)
-
+#This brings in my variables from the nodes used in the 2d section
 export (NodePath) var level1
 export (NodePath) var level2
 onready var button1 = get_node(level1)
 onready var button2 = get_node(level2)
-#onready var TweenNode = get_node("Tween")
-#onready var Position2D = get_node("Position2D")
-#onready var sprite = get_node("Sprite")
 
 func _ready():
-	button1.set_text("Level 1")
-	button1.connect("pressed", self, "on_pressed")
-	button2.set_text("Level 2")
+	button1.set_text("Level 1") #This will show words on the button which the program is launched
+	button1.connect("pressed", self, "on_pressed") #This will connect the button on the screen to the code so that it will work when pressed
+	button2.set_text("Level 2") #This is the same as above but for the second button. Its the same for the code below.
 	button2.connect("pressed", self, "on_pressed2")
 
-func on_pressed():
-		$Tween.interpolate_property($Sprite, "position", Vector2(474,252), Vector2(895,530), 1.0, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-		$Tween.start()
+func on_pressed(): #This is the code that allows the sprite to ove physcially instead of teleporting
+	$Tween.interpolate_property($Sprite, "position", Vector2(474,252), Vector2(895,530), 1.0, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	$Tween.start()
 
-func on_pressed2():
+func on_pressed2(): #The same as above but for the second button
 	$Tween.interpolate_property($Sprite, "position", Vector2(895,530), Vector2(1300,730), 1.0, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	$Tween.start()
